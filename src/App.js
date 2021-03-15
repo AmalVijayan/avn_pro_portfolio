8a22b11348ea25eae639f7d7e9cd4e8ad20f1e80
Amal V Nair <amalvnair95@gmail.com>
2021-03-15 18:06:31 +0530

    Added Blog page INIT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Home from './pages';
+import Blog from './pages/blog';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
@@ -10,7 +12,9 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" component={Home} exact />
+          <Route path="/blog" component={Blog} />
         </Switch>
+    
       </Router>
     </div>
   );
