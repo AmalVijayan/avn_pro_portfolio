a1f6fe07fb068bfd231ea7b9a565aaa8d057b84c
Amal V Nair <amalvnair95@gmail.com>
2021-03-16 14:03:28 +0530

    Auto scroll to top of pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import  NavBar from '../components/NavBar'
 import MobileMenu from '../components/MobileMenu'
 import Footer from '../components/Footer'
 import BlogSection from '../components/BlogSection'
+import ScrollToTop from '../ScrollToTop'
 
 const Blog = () => {
 
@@ -14,7 +15,7 @@ const Blog = () => {
 
 
     return (
-        <> 
+        <>  <ScrollToTop />
             <MobileMenu isOpen={isOpen} toggle={toggle} showScrollMenu={false} showHomeIcon={true}/>
             <NavBar toggle={toggle} showScrollMenu={false} showHomeIcon={true} />
             <BlogSection />
